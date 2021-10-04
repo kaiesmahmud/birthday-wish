06ca4f321002af9e02b1a670c275184886e3d42c
kaiesmahmud <kaiesmahmud@gmail.com>
2021-10-04 12:21:50 +0600

    all done

---
+++ script.js
@@ -0,0 +1,11 @@
+
+ window.open = function() {};
+ window.print = function() {};
+ // Support hover state for mobile.
+ if (false) {
+     window.ontouchstart = function() {};
+ }
+
+if (document.location.search.match(/type=embed/gi)) {
+window.parent.postMessage('resize', "*");
+}
